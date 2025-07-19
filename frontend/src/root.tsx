172e9be54a83c247e79f0bec7ff750cbff78e845
MARCCHERGGI <161167689+MARCCHERGGI@users.noreply.github.com>
2025-07-19 04:37:03 -0400

    Update root.tsx

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -1,16 +1,17 @@
-// src/routes/root.tsx
+// src/root.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./index";
-import InterviewPage from "./interview";
-import ResumePage from "./resume";
+import InterviewPage from "./routes/interview/index";
+import ResumePage from "./routes/resume/index";
+import PaywallPage from "./routes/paywall/index";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<h1 className="text-white">Home</h1>} />
         <Route path="/interview" element={<InterviewPage />} />
         <Route path="/resume" element={<ResumePage />} />
+        <Route path="/paywall" element={<PaywallPage />} />
       </Routes>
     </BrowserRouter>
   );
