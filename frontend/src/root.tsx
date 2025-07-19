583bc0ec91bd31101193aa431f9785405be3f8c6
MARCCHERGGI <161167689+MARCCHERGGI@users.noreply.github.com>
2025-07-19 04:16:51 -0400

    Update root.tsx

--- frontend/src/root.tsx
+++ frontend/src/root.tsx
@@ -1,11 +1,17 @@
-import { component$ } from '@builder.io/qwik';
-import { QwikCity, RouterOutlet } from '@builder.io/qwik-city';
-import './global.css';
+// src/routes/root.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./index";
+import InterviewPage from "./interview";
+import ResumePage from "./resume";
 
-export default component$(() => {
+export default function App() {
   return (
-    <QwikCity>
-      <RouterOutlet />
-    </QwikCity>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/interview" element={<InterviewPage />} />
+        <Route path="/resume" element={<ResumePage />} />
+      </Routes>
+    </BrowserRouter>
   );
-});
+}
